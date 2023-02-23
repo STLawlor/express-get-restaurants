b2e428550b2548736207655d308b0eea270db325
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-02-23 15:14:03 +0000

    add express router

--- server.js
+++ server.js
@@ -1,50 +1,12 @@
 const express = require("express");
-const app = express();
-const { Restaurant } = require("./models/index");
 const { sequelize } = require("./db");
+const { restaurRouter } = require("./routes/restaurant.route")
 
+const app = express();
 const port = 3000;
-app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 
-app.get("/restaurants", async (req, res) => {
-  const restaurants = await Restaurant.findAll();
-  res.json(restaurants);
-});
-
-app.get("/restaurants/:id", async (req, res) => {
-  const restaurant = await Restaurant.findByPk(req.params.id);
-  res.json(restaurant);
-});
-
-app.post("/restaurants", async (req, res) => {
-  try {
-    await Restaurant.create(req.body);
-    res.send("Update complete");
-  } catch (error) {
-    res.status(500).send({ err: error.message });
-  }
-});
-
-app.put("/restaurants/:id", async (req, res) => {
-  try {
-    let restaurant = await Restaurant.findByPk(req.params.id);
-    await restaurant.update(req.body);
-    res.send("Update complete");
-  } catch (error) {
-    res.status(500).send({ err: error.message });
-  }
-});
-
-app.delete("/restaurants/:id", async (req, res) => {
-  try {
-    let restaurant = await Restaurant.findByPk(req.params.id);
-    await restaurant.destroy();
-    res.send("Delete complete");
-  } catch (error) {
-    res.status(500).send({ err: error.message });
-  }
-});
+app.use(express.urlencoded({ extended: true }));
+app.use("/restaurant", restaurRouter);
 
 app.listen(port, () => {
   sequelize.sync();
